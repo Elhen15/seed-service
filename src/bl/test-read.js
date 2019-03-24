c2985f98a4b0734571acadccd7cdb5fef50f3405
Liza Talis <s8267741@MAMDAS>
2019-03-24 16:25:58 +0200

    convert kafka into a modules

--- src/bl/test-read.js
+++
@@ -1,2 +0,0 @@
-const writeToTopic = require('../resources/kafka/read-from-topic');
-readFromTopic('test.t', 'liza', (data) => console.log(data), (err) => console.log(err));
--- src/bl/test-write.js
+++
@@ -1,2 +0,0 @@
-const writeToTopic = require('../resources/kafka/write-to-topic');
-writeToTopic({test: 'test'}, 'test.t').then((data) => console.log('works')).catch((err) => console.log(err));
