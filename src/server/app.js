a3e8a80b0f2fee4c4008fcc6e94ae64e8fab7cac
Liza Talis <s8267741@MAMDAS>
2019-08-28 11:49:58 +0300

    refactor: update README.md

--- src/server/app.js
+++ src/server/app.js
@@ -17,24 +17,6 @@ app.get('/p/:paramVal', [routingFunctions.handleGetWithParams()]);
 
 app.get('/p', [routingFunctions.handleGetWithQuery()]);
 
-/**
- * @swagger
- * /e:
- *      get:
- *              tags:
- *              - node seed
- *              summary: test the error middleware
- *              description: test the error middleware
- *              consumes: application/json
- *              responses:
- *                      500:
- *                              description: 'Something broke'
- */
-// test error middleware
-app.get('/e', (req, res, next) => {
-	throw new Error('Error middleware will catch me');
-});
-
 // needs to be last
 app.use([middlewareFunctions.errorHandlerMiddleware()]);
 
